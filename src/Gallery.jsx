62c824be42a5ded430f1e75d2c8cf46bd1bd7683
roypulak <roy1pulak@gmail.com>
2017-05-28 19:10:11 +0200

    Playing Audio with java script

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Gallery extends Component {
+	playAudio(previewUrl) {
+		console.log('playAudio function called');
+		let audio = new Audio(previewUrl);
+		audio.play();
+	}
+
 	render() {
-		console.log("gallery props", this.props);
 		const { tracks } = this.props;
 		return(
 			<div>
 				{tracks.map((track, k) => {
+					//console.log("Hi look on track", track);
 					const trackImg = track.album.images[0].url;
 					return (
 							<div 
 							key={k} 
-							className="track"
+							className="track" 
+							onClick={() => this.playAudio(track.preview_url)} 
 							>
 								<img 
 									src={trackImg} 
